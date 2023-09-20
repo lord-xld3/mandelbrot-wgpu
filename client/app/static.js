4d49d26e055daa452b01aa5c827ce0d412280e56
Kyle Simmons <simmonskyleb@gmail.com>
2023-09-20 17:17:45 -0500

    Removed webmanifest, using manifest.json

--- client/app/static.js
+++ client/app/static.js
@@ -5,7 +5,6 @@ import "../static/apple-touch-icon.png";
 import "../static/android-chrome-144x144.png";
 import "../static/favicon-32x32.png";
 import "../static/favicon-16x16.png";
-import "../static/site.webmanifest";
 import "../static/safari-pinned-tab.svg";
 import "../static/browserconfig.xml";
 import "../static/favicon.ico";
