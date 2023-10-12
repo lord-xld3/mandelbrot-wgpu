e0a126a37578e2cfde8ce2cca986d1161a664040
Kyle Simmons <simmonskyleb@gmail.com>
2023-10-12 13:40:26 -0500

    less cloning, more borrowing

--- mandelbrot/src/lib.rs
+++ mandelbrot/src/lib.rs
@@ -4,7 +4,6 @@ mod utils;
 #[path = "lib_test.rs"]
 mod lib_test;
 
-use std::iter::repeat;
 use itertools_num::linspace;
 use num::complex::Complex64;
 use wasm_bindgen::prelude::*;
@@ -16,13 +15,13 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // how many iterations does it take to escape?
 fn get_escape_iterations(
-    x: f64,
-    y: f64,
+    x: &f64,
+    y: &f64,
     max_iterations: u32,
     escape_radius: f64,
     exponent: u32,
 ) -> (u32, Complex64) {
-    let c: Complex64 = Complex64::new(x, y); // complex64 is a tuple[f64, f64]
+    let c: Complex64 = Complex64::new(*x, *y);
     let mut z: Complex64 = c;
 
     let mut iter: u32 = 0;
@@ -35,60 +34,28 @@ fn get_escape_iterations(
     (iter, z)
 }
 
-fn check_row<I>(
+fn check_range(
     ref_iter: u32,
-    range: I,
+    re_range: &Vec<f64>,
+    im_range: &Vec<f64>,
     max_iterations: u32,
     escape_radius: f64,
     exponent: u32,
-    mask: &mut Vec<Vec<u32>>,
-    col: usize,
-) -> bool
-where
-    I: Iterator<Item = (f64, f64)>,
-{
-    let mut in_set = true;
+    mask: &mut Vec<Vec<(u32, Complex64)>>,
+) -> bool {
+    let mut all_same = true;
 
-    for (index, pixel) in range.enumerate() {
-        let escape_iterations =
-            get_escape_iterations(pixel.0, pixel.1, max_iterations, escape_radius, exponent).0;
+    for (i, x) in re_range.iter().enumerate() {
+        for (j, y) in im_range.iter().enumerate() {
+            let (iter, complex) = get_escape_iterations(x, y, max_iterations, escape_radius, exponent);
+            mask[i][j] = (iter, complex);
 
-        mask[index][col] = escape_iterations;
-
-        if escape_iterations != ref_iter {
-            in_set = false;
+            if iter != ref_iter {
+                all_same = false;
+            }
         }
     }
-
-    in_set
-}
-
-fn check_col<I>(
-    ref_iter: u32,
-    range: I,
-    max_iterations: u32,
-    escape_radius: f64,
-    exponent: u32,
-    mask: &mut Vec<Vec<u32>>,
-    row: usize,
-) -> bool
-where
-    I: Iterator<Item = (f64, f64)>,
-{
-    let mut in_set = true;
-
-    for (index, pixel) in range.enumerate() {
-        let escape_iterations =
-            get_escape_iterations(pixel.0, pixel.1, max_iterations, escape_radius, exponent).0;
-
-        mask[row][index] = escape_iterations;
-
-        if escape_iterations != ref_iter {
-            in_set = false;
-        }
-    }
-
-    in_set
+    all_same
 }
 
 // map leaflet coordinates to complex plane
@@ -117,15 +84,13 @@ pub fn get_tile(
 
     // Canvas API expects UInt8ClampedArray
     let mut img: Vec<u8> = vec![0; output_size]; // [ r, g, b, a, r, g, b, a, r, g, b, a... ]
-    let mut mask: Vec<Vec<u32>> = vec![vec![0; image_side_length]; image_side_length];
+    let mut mask: Vec<Vec<(u32, Complex64)>> = vec![vec![(0, Complex64::new(0.0, 0.0)); image_side_length]; image_side_length];
 
     let (re_min, im_min) = map_coordinates(center_x, center_y, z, image_side_length);
     let (re_max, im_max) = map_coordinates(center_x + 1.0, center_y + 1.0, z, image_side_length);
 
-    let re_range = linspace(re_min, re_max, image_side_length);
-    let im_range = linspace(im_min, im_max, image_side_length);
-    let re_range_vec: Vec<f64> = re_range.clone().collect();
-    let im_range_vec: Vec<f64> = im_range.clone().collect();
+    let re_range: Vec<f64> = linspace(re_min, re_max, image_side_length).collect();
+    let im_range: Vec<f64> = linspace(im_min, im_max, image_side_length).collect();
 
     let palette_scale_factor = 20.0;
     let scaled_max_iterations = (max_iterations * palette_scale_factor as u32) as usize;
@@ -133,28 +98,33 @@ pub fn get_tile(
     // radius has to be >=3 for color smoothing
     let escape_radius = 3.0;
 
-    // Clone the ranges to get the top-left pixel as a reference
+    // Get the top-left pixel as a reference
     let (ref_iter, ref_complex) = get_escape_iterations(
-        re_range_vec[0],
-        im_range_vec[0],
+        &re_range[0],
+        &im_range[0],
         max_iterations,
         escape_radius,
         exponent,
     );
 
-    let top_result = check_row(ref_iter, re_range.clone().zip(repeat(im_range_vec[0])), max_iterations, escape_radius, exponent, &mut mask, 0);
+    let mut check_closure = |x, y| {
+        check_range(ref_iter, x, y, max_iterations, escape_radius, exponent, &mut mask)
+    };
     
-    let bottom_result = check_row(ref_iter, re_range.clone().zip(repeat(im_range_vec[im_range.len() - 1])), max_iterations, escape_radius, exponent, &mut mask, im_range.len() - 1);
+    let im_start = vec![im_range[0]];
+    let im_end = vec![im_range[im_range.len() - 1]];
+    let re_start = vec![re_range[0]];
+    let re_end = vec![re_range[re_range.len() - 1]];
     
-    let left_result = check_col(ref_iter, repeat(re_range_vec[0]).zip(im_range.clone()), max_iterations, escape_radius, exponent, &mut mask, 0);
-    
-    let right_result = check_col(ref_iter, repeat(re_range_vec[re_range.len() - 1]).zip(im_range.clone()), max_iterations, escape_radius, exponent, &mut mask, re_range.len() - 1);
-
-    let all_true = top_result && bottom_result && left_result && right_result;
+    let all_true =
+        check_closure(&re_range, &im_start) // top
+        && check_closure(&re_range, &im_end) // bottom
+        && check_closure(&re_start, &im_range) // left
+        && check_closure(&re_end, &im_range); // right
 
     if all_true {
-        for (x, _im) in im_range.enumerate() {
-            for (y, _re) in re_range.clone().enumerate() {
+        for (x, _im) in im_range.iter().enumerate() {
+            for (y, _re) in re_range.iter().enumerate() {
                 let pixel:[u8; 3];
                 {
                     // See: https://www.iquilezles.org/www/articles/mset_smooth/mset_smooth.htm
@@ -175,18 +145,16 @@ pub fn get_tile(
             }
         }
     } else {
-        let mut rect_width = image_side_length;
-        let mut rect_height = image_side_length;
-        while rect_width > 6 && rect_height > 6 {  
-            if rect_width >= rect_height {
-                rect_width /= 2;
-                // Check along the horizontal split
-                let result = check_row(ref_iter, re_range.clone().zip(repeat(im_range_vec[rect_width])), max_iterations, escape_radius, exponent, &mut mask, rect_width);
-        
-            } else {
-                rect_height /= 2;
-            }
-        }
+        // The entire tile is not the same color as ref_iter
+        // So we need to recursively sub-divide the tile in half
+        // On each division...
+        // If each rectangle is < 3 in width or height, check unvisited pixels and return that part of the image
+        // Else, divide it in half horizontally or vertically, comparing width >= height
+            // Then, get a new reference pixel from the top-left of each new rectangle
+            // Then, check along the division row/column
+            // If this is the same color as the reference pixel, check the rest of the borders using the mask
+                // If the entire border is the same color, fill the rectangle with that color
+            // Else, divide the rectangle in half again
     }
 
     img
